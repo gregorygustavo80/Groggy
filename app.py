517f3e3dcfb3e5d2d3226e395a2cc133ba290606
Gregory <gregorygustavo@hotmail.com>
2025-04-17 12:53:04 -0300

    update

--- app.py
+++ app.py
@@ -28,4 +28,8 @@ def index():
     return render_template("index.html", resultados=resultados, hora_dormir=hora_dormir_str)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
